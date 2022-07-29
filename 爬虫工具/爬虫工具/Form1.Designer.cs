d93856c10f4f5da8bea41f866a06c69bcfd97668
TextAccount <75971727+CodeEngieer@users.noreply.github.com>
2022-07-29 10:29:39 +0800

    7.29 commit

---
+++ 爬虫工具/爬虫工具/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace 爬虫工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Website = new System.Windows.Forms.TextBox();
+            this.textBox_SavePath = new System.Windows.Forms.TextBox();
+            this.button_OpenFile = new System.Windows.Forms.Button();
+            this.listBox_ShowMessage = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(31, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请选择保存的文件夹：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(31, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "请输入爬取图片的网址：";
+            // 
+            // textBox_Website
+            // 
+            this.textBox_Website.Location = new System.Drawing.Point(210, 132);
+            this.textBox_Website.Name = "textBox_Website";
+            this.textBox_Website.Size = new System.Drawing.Size(389, 25);
+            this.textBox_Website.TabIndex = 4;
+            this.textBox_Website.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_Website_KeyPress);
+            // 
+            // textBox_SavePath
+            // 
+            this.textBox_SavePath.Location = new System.Drawing.Point(210, 88);
+            this.textBox_SavePath.Name = "textBox_SavePath";
+            this.textBox_SavePath.Size = new System.Drawing.Size(388, 25);
+            this.textBox_SavePath.TabIndex = 5;
+            // 
+            // button_OpenFile
+            // 
+            this.button_OpenFile.Location = new System.Drawing.Point(621, 88);
+            this.button_OpenFile.Name = "button_OpenFile";
+            this.button_OpenFile.Size = new System.Drawing.Size(48, 25);
+            this.button_OpenFile.TabIndex = 6;
+            this.button_OpenFile.Text = "...";
+            this.button_OpenFile.UseVisualStyleBackColor = true;
+            this.button_OpenFile.Click += new System.EventHandler(this.button_OpenFile_Click);
+            // 
+            // listBox_ShowMessage
+            // 
+            this.listBox_ShowMessage.FormattingEnabled = true;
+            this.listBox_ShowMessage.ItemHeight = 15;
+            this.listBox_ShowMessage.Location = new System.Drawing.Point(34, 172);
+            this.listBox_ShowMessage.Name = "listBox_ShowMessage";
+            this.listBox_ShowMessage.Size = new System.Drawing.Size(662, 394);
+            this.listBox_ShowMessage.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(621, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox_ShowMessage);
+            this.Controls.Add(this.button_OpenFile);
+            this.Controls.Add(this.textBox_SavePath);
+            this.Controls.Add(this.textBox_Website);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "爬虫工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Website;
+        private System.Windows.Forms.TextBox textBox_SavePath;
+        private System.Windows.Forms.Button button_OpenFile;
+        private System.Windows.Forms.ListBox listBox_ShowMessage;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 爬虫工具/爬虫工具/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace 爬虫工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_OpenFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog saveUrl = new FolderBrowserDialog();
+            saveUrl.RootFolder = Environment.SpecialFolder.Desktop;
+            saveUrl.Description = "";
+            var result = saveUrl.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string savePath = saveUrl.SelectedPath;
+                textBox_SavePath.Text = savePath;
+            }
+        }
+        /*
+         * 方法名称：HttpGetHandle（获取网络请求）
+         * 方法功能：爬取指定网站
+         * @param：string：url：指定网址
+         * @param：string：path：保存地址
+         * @param：int：name：图片递归名字
+         */
+        public void HttpGetHandle(string url, string path, int name)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            HttpWebRequest webRequest = WebRequest.CreateHttp(url);
+            webRequest.Method = "GET";
+            webRequest.UserAgent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0";
+            var webResponse = webRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+            string str = streamReader.ReadToEnd();
+            streamReader.Close();
+            if (string.IsNullOrEmpty(str))
+            {
+                listBox_ShowMessage.Items.Add("=====================错误====================");
+
+            }
+            //正则表达式配对网站图片
+            Regex regex = new Regex(@"<img\b[^<>]*?\bsrc[\s\t\r\n]*=[\s\t\r\n]*[""']?[\s\t\r\n]*(?<Group>[^\s\t\r\n""'<>]*)[^<>]*?/?[\s\t\r\n]*>");
+            MatchCollection match = regex.Matches(str);
+            WebClient client = new WebClient();
+            int temp = 0;
+            try
+            {
+                foreach (Match match1 in match)
+                {
+                    string src = match1.Groups[1].Value;
+                    if (src.Contains("http") && !src.Contains(".svg"))
+                    {
+                        temp++;
+                        client.DownloadFile(src, path + name + ".jpg");
+                        name++;
+                        listBox_ShowMessage.Items.Add("\n正在爬取=================" + "|" + temp);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox_ShowMessage.Items.Add("===============" + ex.ToString());
+            }
+            stopwatch.Stop();
+            listBox_ShowMessage.Items.Add("===================爬取成功====================");
+            listBox_ShowMessage.Items.Add("\n_______总共爬取了" + temp + "张图片!_______________");
+            listBox_ShowMessage.Items.Add("\n一共耗时" + stopwatch.ElapsedMilliseconds / 1000 + "秒");
+        }
+        private void CreateFile()
+        {
+            if (Directory.Exists(@"D:\Picture\"))
+            {
+                listBox_ShowMessage.Items.Add("开始爬取指定网站图片！");
+            }
+            else
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(@"D:\Picture\");
+                directoryInfo.Create();
+            }
+        }
+        private void textBox_Website_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (textBox_Website != null)
+                {
+                    CreateFile();
+                    string savePath = textBox_SavePath.Text;
+                    string completePath = savePath + "//";
+                    string url = textBox_Website.Text;
+                    HttpGetHandle(url, completePath, 1);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string savePath = textBox_SavePath.Text;
+            string completePath = savePath + "//";
+            string url = textBox_Website.Text;
+            HttpGetHandle(url, completePath, 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_Website.Text = "https://codeengieer.github.io/BufferPrivateBlog.github.io/page/2/";
+        }
+    }
+}
